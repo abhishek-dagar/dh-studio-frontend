cb8f37562197227ca6751159458350dcf2875790
abhishek-dagar <dagarabishek584@gmail.com>
2025-01-12 20:28:21 +0530

    fixed build issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
           numSquares={30}
           maxOpacity={0.1}
           duration={3}
-          repeatDelay={1}
+          // repeatDelay={1}
           className={cn(
             "[mask-image:radial-gradient(500px_circle_at_center,white,transparent)]",
             "inset-x-0 inset-y-[-30%] h-[200%] skew-y-12",
